f1bbd1ae72381d84e2c5e98fee7017e6bedc8129
chiragbidx <chirag@bidx.ai>
2026-03-14 16:15:18 +0530

    Delete legacy CommonJS next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: [],
-  },
-};
-
-module.exports = nextConfig;
+<!--DELETE FILE-->
